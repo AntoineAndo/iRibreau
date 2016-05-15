31d6ab3deed17c4c5a56f82635f5c5487f004986
Alexandre <xaa-888@hotmail.fr>
2016-05-15 16:57:03 +0200

    DAL & project restructuring

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	'instagramAuth' : {
-	    clientID: 'cab1217ca1d448fdb4b93f4b64495fff',
-	    clientSecret: 'b840e8a1ef9542c19719d98d356c862f',
-	    callbackURL: "http://127.0.0.1:8080/auth/instagram/callback"
-	}
-}
